978c809c76d38e2e30dcc33a3e07c39076f39514
Primata <primata@movementlabs.xyz>
2024-06-03 18:11:04 -0300

    push default devnet

--- src/constants.tsx
+++ src/constants.tsx
@@ -37,7 +37,7 @@ for (const key of Object.keys(networks)) {
   }
 }
 
-export const defaultNetworkName: NetworkName = "local" as const;
+export const defaultNetworkName: NetworkName = "devnet" as const;
 
 if (!(defaultNetworkName in networks)) {
   throw `defaultNetworkName '${defaultNetworkName}' not in Networks!`;
